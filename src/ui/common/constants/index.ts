8d69219038f7a7aa66480b01414d2ebf1bb95307
crazyhead1 <106295363+crazyhead1@users.noreply.github.com>
2022-06-10 13:37:22 +0500

    initial commit; Typescript configuration

---
+++ src/ui/common/constants/index.ts
@@ -0,0 +1,1 @@
+export const CONSTANT_NAME = "Constant value";
---
+++ src/ui/pos-engine/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { classes } from './POSEngine'
+
+export const POSEngine = () => {
+  return (
+    <div>POSEngine</div>
+  )
+}
